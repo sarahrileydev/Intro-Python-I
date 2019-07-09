462caa6f179001d2a17552c250acd32025afa3fd
sriley <sarahriley.dev@gmail.com>
2019-07-09 19:20:31 -0400

    calendar

--- src/14_cal.py
+++ src/14_cal.py
@@ -22,3 +22,23 @@ and does the following:
 import sys
 import calendar
 from datetime import datetime
+
+t = datetime.now()
+month = t.month
+year = t.year
+
+
+args = sys.argv[1:]
+
+if args:
+    month = int(args[0])
+    if len(args) > 1:
+        year = int(args[0])
+        month = int(args[1])
+
+if len(args) > 2:
+    print("Input must be YYYY MM format.")
+elif month < 1 or 12 < month:
+    print("Month must be 1 to 12")
+else:
+    print(calendar.month(year, month)) 
